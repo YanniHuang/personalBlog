9318b719a0e751e297db468fdce0c798aed2daee
Emily Huang <emily@arkade.com.au>
2017-10-18 10:05:07 +1100

    add auth

--- database/migrations/2017_10_17_222717_create_categories_table.php
+++ database/migrations/2017_10_17_222717_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('display_name');
-            $table->integer('parent_id');
+            $table->integer('parent_id')->default(0);
             $table->string('content');
             $table->timestamps();
         });
